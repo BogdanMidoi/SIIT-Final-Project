75a8551face7f73612588b1106a2d81ed4aa6336
Bogdan <midoi_bogdan29bv@yahoo.com>
2019-04-24 20:49:12 +0300

    initial commit

---
+++ src/main/java/SIIT/Final/Project/FinalProjectApplication.java
@@ -0,0 +1,13 @@
+package SIIT.Final.Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalProjectApplication.class, args);
+	}
+
+}
